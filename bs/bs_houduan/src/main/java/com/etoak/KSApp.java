701c10256f921e586a7ac472bc0c4924b4ec6009
voika <2721688374@qq.com>
2022-05-19 09:49:04 +0800

    bs启动类里加了日志信息

--- bs/bs_houduan/src/main/java/com/etoak/KSApp.java
+++ bs/bs_houduan/src/main/java/com/etoak/KSApp.java
@@ -1,14 +1,18 @@
 package com.etoak;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.etoak.mapper")
+@Slf4j
 public class KSApp {
     public static void main(String[] args) {
+        log.info("项目正在启动...");
         SpringApplication.run(KSApp.class,args);
+        log.info("项目启动成功");
     }
 
 }
